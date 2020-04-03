020dbb6ba84b7a2d1b21efb217e8c675fb9a82bb
Yann Klein <yann.klein@me.com>
2020-04-03 16:26:18 +0900

    init of i18n with fr and de

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :location, :photo])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 end
