b3c22c7f835e26f0ea524899f6fa3a38bc94fb21
Yann Klein <yann.klein@me.com>
2023-01-25 12:39:43 +0900

    changed has_one_attached to accept differently named DB

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,10 @@ class Recipe < ApplicationRecord
   belongs_to :country, foreign_key: "recettes_de_mos_country_id"
   belongs_to :category, foreign_key: "recettes_de_mos_category_id"
   belongs_to :user
-  has_one_attached :photo
+  # has_one_attached :photo
+  def photo
+    attachement = ActiveStorage::Attachment.find_by(name: :photo, record_type: 'RecettesDeMosRecipe', record_id: self.id)
+    attachement.define_singleton_method(:attached?) { !!attachement.key }
+    attachement
+  end
 end
